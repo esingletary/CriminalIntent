558c0cbe202ec20c831f0e4d521e36bc18e6830d
Emmanuel Singletary <me@manny.co>
2017-12-07 23:58:03 -0500

    Added Chapter 14 features.

---
+++ app/src/main/java/co/manny/bnr/criminalintent/database/CrimeCursorWrapper.java
@@ -0,0 +1,37 @@
+package co.manny.bnr.criminalintent.database;
+
+import android.database.Cursor;
+import android.database.CursorWrapper;
+
+import java.util.Date;
+import java.util.UUID;
+
+import co.manny.bnr.criminalintent.Crime;
+import co.manny.bnr.criminalintent.database.CrimeDbSchema.CrimeTable;
+
+/**
+ * Created by Emmanuel on 12/7/2017.
+ */
+
+public class CrimeCursorWrapper extends CursorWrapper {
+
+    public CrimeCursorWrapper(Cursor cursor) {
+        super(cursor);
+    }
+
+    public Crime getCrime() {
+        String uuidString = getString(getColumnIndex(CrimeTable.Cols.UUID));
+        String title = getString(getColumnIndex(CrimeTable.Cols.TITLE));
+        long date = getLong(getColumnIndex(CrimeTable.Cols.DATE));
+        int isSolved = getInt(getColumnIndex(CrimeTable.Cols.SOLVED));
+
+        Crime crime = new Crime (UUID.fromString(uuidString));
+        crime.setTitle(title);
+        crime.setDate(new Date(date));
+        crime.setSolved(isSolved != 0);
+
+        return crime;
+    }
+
+
+}
---
+++ app/src/main/java/co/manny/bnr/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,20 @@
+package co.manny.bnr.criminalintent.database;
+
+/**
+ * Created by Emmanuel on 12/7/2017.
+ */
+
+public class CrimeDbSchema {
+
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
+
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+
+}
